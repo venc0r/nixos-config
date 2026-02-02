7cedcbf7bee2062c4cdd129ac738c925ad5f7e09
Jörg Markert <venc0r@live.com>
2026-02-02 01:38:58 +0100

    works on my maschine

--- flake.nix
+++ flake.nix
@@ -1,29 +1,27 @@
 {
     inputs = {
-        # This is pointing to an unstable release.
-        # If you prefer a stable release instead, you can this to the latest number shown here: https://nixos.org/download
-        # i.e. nixos-24.11
-        # Use `nix flake update` to update the flake to the latest revision of the chosen release channel.
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    outputs = inputs@{ self, nixpkgs, ... }:
+    outputs = { self, nixpkgs, ... } @ inputs:
         let
             system = "x86_64-linux";
-            pkgs = import nixpkgs {
+	    pkgs = import nixpkgs {
                 inherit system;
-                config = {
-                    allowUnfree = true;
-                };
+                config = { allowUnfree = true; };
             };
         in
             {
-            # NOTE: 'nixos' is the default hostname
             nixosConfigurations = {
                 nixos = nixpkgs.lib.nixosSystem {
-                    specialArgs = { inherit system; };
+                    specialArgs = { inherit system; inherit inputs; };
                     modules = [
-                        ./nixos/configuration.nix
+                        ./hosts/nixos/configuration.nix
+ 			inputs.home-manager.nixosModules.default
                     ];
                 };
             };
