ca58a10e354ee4e18e0701a7cdd0cbe83a36cb98
Jörg Markert <venc0r@live.com>
2026-02-04 21:06:24 +0100

    fix: update sddm configuration to new option path

--- hosts/common.nix
+++ hosts/common.nix
@@ -17,9 +17,6 @@
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # networking.hostName is set in per-host configuration
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
@@ -60,9 +57,10 @@
       layout = "us";
       variant = "altgr-intl";
     };
-    displayManager.sddm = {
-      enable = true;
-    };
+  };
+
+  services.displayManager.sddm = {
+    enable = true;
   };
 
   # Define a user account.
@@ -86,32 +84,9 @@
 
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
-    users = {
-      # We assume home.nix is in the same directory as the host config
-      # But since we moved this to common, we need to be careful about paths.
-      # Ideally home.nix should also be common or passed as an argument.
-      # For now, let's point to a common home.nix or handle this carefully.
-      # The original code was: "jma" = import ./home.nix;
-
-      # Since home.nix is currently inside hosts/nixos/home.nix and hosts/cubi/home.nix,
-      # we need to decide if we centralize home.nix too.
-      # For this step, I will point to the one in hosts/nixos as a temporary common,
-      # OR we can leave the home-manager block in the per-host config?
-
-      # DECISION: I will comment this out here and suggest we keep the home-manager
-      # import in the per-host file for a moment, OR I can fix the path.
-      # Let's keep it in common but use a relative path that works if we move home.nix too.
-      # actually, let's try to reference the one in the host directory.
-      # "jma" = import ./home.nix; <- This won't work if common.nix is in hosts/common.nix
-
-      # Let's temporarily REMOVE the user import from here and put it back in the host config
-      # to avoid path breakage until we decide where home.nix lives.
-    };
+    # User config is imported in per-host configuration
+    users = { };
   };
-
-  # REVISION: To keep it simple, I will keep home-manager generic settings here,
-  # but the actual 'users.jma = import ...' line I will put in the host config for now
-  # to avoid "file not found" errors.
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
