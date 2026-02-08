14d8ed9936d321de6b696d7dcfa8f617ff424c40
Jörg Markert <venc0r@live.com>
2026-02-08 17:34:09 +0100

    temp: disable zoom-us due to download hang
    - Zoom download from zoom.us servers hangs at 0 bytes
    - Need to troubleshoot separately (possible network/timeout issue)
    - Temporarily disabled to allow other packages to deploy
    - Will investigate alternative installation methods

--- hosts/cubi/packages.nix
+++ hosts/cubi/packages.nix
@@ -5,6 +5,6 @@
   home.packages = with pkgs; [
     # Communication & collaboration tools
     teams-for-linux # Microsoft Teams client
-    zoom-us # Zoom video conferencing
+    # zoom-us # Zoom - DISABLED: download hangs, troubleshoot separately
   ];
 }
