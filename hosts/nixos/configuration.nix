cbd4ca7fe16ff3dd8b2889a224671803e049c4c8
Jörg Markert <joerg.markert@noris.de>
2026-02-23 19:04:10 +0100

    with 2 accounts

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -13,8 +13,15 @@
 
   networking.hostName = "nixos";
 
-  # Home Manager configuration
-  home-manager.users.jma.imports = [
-    ../home.nix # Shared config
-  ];
+  home-manager.users.jma = {
+    imports = [ ../home.nix ];
+    home.username = lib.mkDefault "jma";
+    home.homeDirectory = lib.mkDefault "/home/jma";
+  };
+
+  home-manager.users."jmarkert@cloudpunks.de" = {
+    imports = [ ../home.nix ];
+    home.username = lib.mkForce "jma@cloudpunks.de";
+    home.homeDirectory = lib.mkForce "/home/jma@cloudpunks.de";
+  };
 }
