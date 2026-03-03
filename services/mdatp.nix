8585f138ba50462850c1e15f2a8079397266cc7e
Jörg Markert <joerg.markert@noris.de>
2026-03-03 15:45:30 +0100

    inherit old, to keep updates

--- services/mdatp.nix
+++ services/mdatp.nix
@@ -6,8 +6,7 @@
     (final: prev: {
       mdatp = prev.mdatp.overrideAttrs (oldAttrs: {
         src = pkgs.fetchurl {
-          url = oldAttrs.src.url or "https://packages.microsoft.com/debian/12/prod/pool/main/m/mdatp/mdatp_101.25012.0000_amd64.deb";
-          hash = oldAttrs.src.outputHash or "sha256-EBnfz4z1t4jwGPKZIKTK1TFacV3UA3BAD1lS+ixs2TE=";
+          inherit (oldAttrs.src) url outputHash outputHashAlgo;
           curlOptsList = [ "--cacert" "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
         };
       });
